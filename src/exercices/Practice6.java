435eb9c2e70bf995d69ba40a3d70d67d9b0d979c
Alonso-dev651 <marcialpebell@gmail.com>
2026-01-07 07:06:11 -0500

    Ejercicio 6

---
+++ src/exercices/Practice6.java
@@ -0,0 +1,22 @@
+package exercices;
+
+import java.util.Scanner;
+
+public class Practice6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese su edad:");
+        int edad = scanner.nextInt();
+
+        if (edad < 12) {
+            System.out.println("Clasificado como Niño");
+        } else if (edad <= 17) {
+            System.out.println("Clasificado como Adolecente");
+        } else if (edad <= 59) {
+            System.out.println("Clasificado como Adulto");
+        } else {
+            System.out.println("Clasificado como Anciano");
+        }
+    }
+}
