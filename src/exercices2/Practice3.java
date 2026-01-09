2218dfc6990302817258276a9c4cdbfa971bc031
Alonso-dev651 <marcialpebell@gmail.com>
2026-01-08 22:50:48 -0500

    Ejercicio 3

---
+++ src/exercices2/Practice3.java
@@ -0,0 +1,12 @@
+package exercices2;
+
+public class Practice3 {
+    public static void main(String[] args) {
+        System.out.println("Numero del 10 al 1 - Descendente");
+
+        for (int i = 10; i >= 1; i--) {
+            if (i == 1) System.out.print(i);
+            else System.out.print(i + " ");
+        }
+    }
+}
