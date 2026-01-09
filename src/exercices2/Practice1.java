4deaa683b91c4d0e20a07de66cc9bf5d4c2f3235
Alonso-dev651 <marcialpebell@gmail.com>
2026-01-08 22:38:40 -0500

    Ejercicio 1

---
+++ src/exercices2/Practice1.java
@@ -0,0 +1,11 @@
+package exercices2;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        System.out.println("Numeros del 1 al 10");
+        for (int i = 1; i <= 10; i++) {
+            if (i == 10) System.out.print(i);
+            else System.out.print(i + " ");
+        }
+    }
+}
