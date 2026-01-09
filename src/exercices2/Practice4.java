8556b98339344067aecadfac81eca6f219f725a1
Alonso-dev651 <marcialpebell@gmail.com>
2026-01-08 23:13:04 -0500

    Correcion Ejercicio 4

--- src/exercices2/Practice4.java
+++ src/exercices2/Practice4.java
@@ -4,11 +4,10 @@ public class Practice4 {
     public static void main(String[] args) {
         System.out.println("Numeros pares del 1 al 20");
 
-        for (int i = 1; i <= 20; i++) {
-            if (i % 2 == 0) {
-                if (i == 20) System.out.print(i);
-                else System.out.print(i + " ");
-            }
+        int i = 1;
+        while (i <= 20) {
+            if (i % 2 == 0) System.out.print(i + " ");
+            i++;
         }
     }
 }
