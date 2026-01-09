f05267b4e29831202f4b650a233bbee1105edae0
Alonso-dev651 <marcialpebell@gmail.com>
2026-01-08 22:47:58 -0500

    Ejercicio 2

---
+++ src/exercices2/Practice2.java
@@ -0,0 +1,14 @@
+package exercices2;
+
+public class Practice2 {
+    public static void main(String[] args) {
+        System.out.println("Suma de numeros del 1 al 5");
+
+        int suma = 0;
+        for (int i = 1; i <= 5; i++) {
+            suma += i;
+        }
+
+        System.out.println("La suma es: " + suma);
+    }
+}
